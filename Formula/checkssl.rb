4dec2346883976275b0ea59af1d8f8d46b807ea5
goreleaserbot <goreleaser@zazeski.com>
2022-10-03 23:12:42 -0500

    Brew formula update for checkssl version v0.4.4

--- Formula/checkssl.rb
+++ Formula/checkssl.rb
@@ -5,21 +5,21 @@
 class Checkssl < Formula
   desc "Simple tool to check or monitor TLS/SSL certificates are working."
   homepage "https://www.checkssl.org/"
-  version "0.4.3"
+  version "0.4.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/szazeski/checkssl/releases/download/v0.4.3/checkssl_0.4.3_Darwin_arm64.tar.gz"
-      sha256 "d616826e6a712d6c8b5f451461a7bbbd253391fcf7a60b900660d3f277792c26"
+      url "https://github.com/szazeski/checkssl/releases/download/v0.4.4/checkssl_0.4.4_Darwin_arm64.tar.gz"
+      sha256 "670633523982f80f6e7422fe1b18d0c386e2cb50f1f3518977decdfb88d9cb50"
 
       def install
         bin.install "checkssl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/szazeski/checkssl/releases/download/v0.4.3/checkssl_0.4.3_Darwin_x86_64.tar.gz"
-      sha256 "382a45a75555d5afd282624809949d705c5c67e79b19b5666b70881cd2c4833c"
+      url "https://github.com/szazeski/checkssl/releases/download/v0.4.4/checkssl_0.4.4_Darwin_x86_64.tar.gz"
+      sha256 "5dd54c8fb984d4caefde84ca1e832987a1e4db1ad3111590efb3931d88a99776"
 
       def install
         bin.install "checkssl"
@@ -29,25 +29,22 @@ class Checkssl < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/szazeski/checkssl/releases/download/v0.4.3/checkssl_0.4.3_Linux_arm64.tar.gz"
-      sha256 "1f30d43ab120b3117ac0a482a1a53be6eff1fab8207e0801d18b2b5be645874e"
+      url "https://github.com/szazeski/checkssl/releases/download/v0.4.4/checkssl_0.4.4_Linux_arm64.tar.gz"
+      sha256 "ef9c112e527b1740a02ba9d66f9e114be2c4cd7d1e61e7672962e080b448e2ae"
 
       def install
         bin.install "checkssl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/szazeski/checkssl/releases/download/v0.4.3/checkssl_0.4.3_Linux_x86_64.tar.gz"
-      sha256 "d9cd8799e876824e127a78ecda2b6b0cbcf4fb45661e2e400c18f26eec67bf7d"
+      url "https://github.com/szazeski/checkssl/releases/download/v0.4.4/checkssl_0.4.4_Linux_x86_64.tar.gz"
+      sha256 "800f38d783709ecbf71e19b48736f2401731e81c59b03b824724eb28eea209b9"
 
       def install
         bin.install "checkssl"
       end
     end
   end
-
-  depends_on "git"
-  depends_on "go"
 
   test do
     system "#{bin}/checkssl checkssl.org"
